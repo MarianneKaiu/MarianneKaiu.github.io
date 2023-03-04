c48bff2201df941af96ff9a663784c548cb31496
MarianneKaiu <lapierre.marianne@gmail.com>
2023-03-04 10:43:45 +0100

    fix nav modal & few z-index

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ let y = 0;
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > y) {
-        nav.style.top = "-60px";
+        nav.style.top = "-400px";
     } else nav.style.top = 0;
     y = window.scrollY;
 });
